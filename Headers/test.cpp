ab42bdbb448b101278074b1b793283070aac858b
FredH <1515435570@qq.com>
2021-12-08 00:04:38 +0800

    simple tests up to clear()

--- Headers/test.cpp
+++ Headers/test.cpp
@@ -1,5 +1,6 @@
 #include "Vector.h"
 #include <iostream>
+#include <string>
 
 int main(){
     MyStl::Vector<int> v{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
@@ -19,16 +20,27 @@ int main(){
 
     std::cout << std::endl;
 
-    v = v3;
-    try{
-        v.at(0) = 2;
-        std::cout << v.at(0) << std::endl;
-    }catch(std::out_of_range const& ex){
-        std::cout << ex.what() << std::endl;
-    }
+    // v = v3;
+    // try{
+    //     v.at(0) = 2;
+    //     std::cout << v.at(0) << std::endl;
+    // }catch(std::out_of_range const& ex){
+    //     std::cout << ex.what() << std::endl;
+    // }
     
-    v3.assign({100, 100, 100, 100, 100});
-    std::cout << v3.size() << " " << v3.capacity() << std::endl;
-    v3.shrink_to_fit();
-    std::cout << v3.size() << " " << v3.capacity() << std::endl;
+    std::string s("abcdefg");
+    v2.assign(6, 'g');
+    std::cout << v2.size() << " " << v2.capacity() << std::endl;
+    v2.shrink_to_fit();
+    std::cout << v2.size() << " " << v2.capacity() << std::endl;
+    v.erase(v.begin(), v.begin() + 3);
+    v.erase(v.end() - 4);
+    for (auto& c : v){
+        std::cout << c << " ";
+    }
+
+    std::cout << std::endl;
+    v.clear();
+    std::cout << (v.empty() ? "empty" : "still not empty...") << std::endl;
+    
 }
