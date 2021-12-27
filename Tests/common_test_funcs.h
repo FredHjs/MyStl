390fdc9aa175947ae049bc7a5700fedf003614a7
FredH <1515435570@qq.com>
2021-12-27 00:08:36 -0500

    initiate tests

--- Tests/common_test_funcs.h
+++ Tests/common_test_funcs.h
@@ -5,8 +5,16 @@
 using std::cout;
 using std::endl;
 
-template<typename Container> void print(Container& container, std::string container_name){
-    cout << container_name << ": ";
-    for (auto &e : container){cout << e << " ";}
-    cout << endl;
+namespace MyStl{
+namespace Tests{
+
+    template<typename Container> void 
+    print(Container& container, std::string container_name){
+        cout << container_name << ": ";
+        for (auto &e : container){cout << e << " ";}
+        cout << endl;
+    }
+    
 }
+}
+
---
+++ Tests/vector_test.cpp
@@ -0,0 +1,24 @@
+#include "common_test_funcs.h"
+#include "Vector.h"
+
+namespace MyStl
+{
+namespace Tests
+{
+    int main(){
+        Vector<int> v1;
+        Vector<char> v2(9, 'a');
+        Vector<std::string> v3(8);
+        Vector<char> v4(v2.begin(), v2.begin() + 3);
+        Vector<std::string> v5(v3);
+        Vector<char> v6(std::move(v4));
+        Vector<std::string> v7{"hello", "world", "hello", "universe"};
+
+        Vector<char> v8, v9;
+        Vector<int> v10;
+        v8 = v2;
+        v9 = std::move(v2);
+        v10 = {1,2,3,4,5,6,7,8,9};
+    }
+} 
+} 
