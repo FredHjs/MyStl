c32be80dee11228e1da4a85a830d1a7510bc796a
FredH <1515435570@qq.com>
2021-12-28 00:05:37 -0500

    scrap for algorithm

---
+++ Headers/Algorithm.h
@@ -0,0 +1,16 @@
+#ifndef MYSTL_ALGORITHM_H
+#define MYSTL_ALGORITHM_H
+
+namespace MyStl
+{
+template<typename InputIt1, typename InputIt2> bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2){
+    for (; first1 != last1; ++first1, ++first2){
+        if (*first1 != *first2) return false;
+    }
+
+    return true;
+}
+} // namespace MyStl
+
+
+#endif
