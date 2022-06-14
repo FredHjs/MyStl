7008958d789723f4526b4f9c0d9c15b0ca72995b
Fred Huang <1515435570@qq.com>
2022-06-14 00:15:08 -0400

    start ctors

--- Headers/List.h
+++ Headers/List.h
@@ -2,6 +2,8 @@
 #define LIST_H
 
 #include "Iterator.h"
+#include <cassert>
+#include <memory>
 
 namespace MyStl{
     template<typename T> struct Node;
@@ -33,22 +35,112 @@ namespace MyStl{
         value_type _val;
     };
 
-    template<typename T>
-    class List_Iterator: public MyStl::Iterator<Bidirectional_Iterator_Tag, T>{
+    template<typename T, typename Pointer, typename Reference>
+    class List_Iterator: public MyStl::Iterator<Bidirectional_Iterator_Tag, T, ptrdiff_t, Pointer, Reference>{
         public:
             using value_type = T;
-            using reference = T&;
-            using pointer = T*;
-            using base_pointer = Node_Base<T>*;
-            using node_pointer = Node<T>*;
+            using reference = Reference;
+            using pointer = Pointer;
+            using base_ptr = Node_Base<T>*;
+            using node_ptr = Node<T>*;
 
         private:
-            base_pointer _node;
+            base_ptr _node;
 
         public:
             List_Iterator() = default;
 
-            
+            List_Iterator(const List_Iterator& other): _node(other._node){}
+
+            List_Iterator(List_Iterator&& other): _node(other._node){other._node = nullptr;}
+
+            List_Iterator(base_ptr bp): _node(bp){}
+
+            List_Iterator(node_ptr np): _node(np->as_base()){}
+
+            T& operator*() const {return _node->as_node()->_val;}
+
+            T* operator->() const {return &(operator*());}
+
+            List_Iterator& operator++(){
+                assert(_node);
+                _node = _node->_next;
+                return *this;
+            }
+
+            List_Iterator& operator--(){
+                assert(_node);
+                _node = _node->_previous;
+                return *this;
+            }
+
+            List_Iterator operator++(int){
+                assert(_node);
+                auto temp = *this;
+                _node = _node->_next;
+                return temp;
+            }
+
+            List_Iterator operator--(int){
+                assert(_node);
+                auto temp = *this;
+                _node = _node->_previous;
+                return temp;
+            }
+
+            bool operator==(const List_Iterator& rhs) const {
+                return _node == rhs._node;
+            }
+
+            bool operator != (const List_Iterator& rhs) const {
+                return _node != rhs._node;
+            }
+    };
+
+    /* circular doubly-linked list, maintains a pointer to the pass-the-end sentinel node */
+    template<typename T>
+    class List{
+        public:
+        /* type defs */
+            using value_type = T;
+            using allocator_type = std::allocator<T>;
+            using size_type = std::size_t;
+            using difference_type = std::ptrdiff_t;
+            using reference = value_type&;
+            using const_reference = const value_type&;
+            using pointer = typename allocator_type::pointer;
+            using const_pointer = typename allocator_type::const_pointer;
+            using iterator = List_Iterator<T, T*, T&>;
+            using const_iterator = List_Iterator<T, const T*, const T&>;
+            using reverse_iterator = Reverse_Iterator<Iterator>;
+            using const_reverse_iterator = Reverse_Iterator<const_iterator>;
+
+            using base_ptr = Node_Base<T>*;
+            using node_ptr = Node<T>*;
+            using node_allocator = std::allocator<Node>;
+
+            allocator_type _get_al() {return allocator_type();}
+            node_allocator _get_node_al() {return node_allocator();}
+
+        private:
+            base_ptr _end;  //pass-the-end sentinel node
+            size_type _size;
+
+        public:
+        /* ctor and dtor */
+        list(): _end(nullptr), _size(0){}
+
+        private:
+        /* helpers */
+        void fill_init_n(size_type count, const value_type& value){
+
+        }
+
+        template <typename... Args>
+        node_ptr create_node(base_ptr at, Args&&...args){
+
+        }
+
     };
 }
 
