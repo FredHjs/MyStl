0bebaad34a2513eef08d7c35bc6e9a6eca0e3bcb
Fred Huang <j462huan@uwaterloo.ca>
2024-02-12 23:31:41 -0500

    tree iterator class

--- Headers/tree.h
+++ Headers/tree.h
@@ -1,5 +1,5 @@
 /* This header file implements RB tree, an internal data structure for MtStl. */
-
+#include "Iterator.h"
 namespace MyStl
 {
     enum _RB_tree_color {
@@ -56,5 +56,117 @@ namespace MyStl
             void _flip_color() {
                 _color = (_color == _RB_tree_color::_black) ? _RB_tree_color::_red : _RB_tree_color::_black;
             }
+    };
+
+    template <class T>
+    class _RB_tree_node : public _RB_tree_node_base {
+        public:
+            typedef _RB_tree_node<T>* _Node_ptr;
+
+            T _val;
+
+            T* _val_ptr() {
+                return std::__addressof(_val);
+            }
+
+            const T* _val_ptr() const {
+                return std::__addressof(_val);
+            } 
+
+    };
+
+    template <class T, class pointer, class reference>
+    class _RB_tree_iterator : public MyStl::Iterator<MyStl::Bidirectional_Iterator_Tag, T, ptrdiff_t, pointer, reference> {
+        public:
+            using _Base_ptr = typename _RB_tree_node_base::_Base_ptr;
+            using _Node_Ptr = _RB_tree_node<T>*;
+            using _Self = _RB_tree_iterator<T>;
+
+            _Base_ptr _node_base;
+
+            _RB_tree_iterator() = default;
+
+            _RB_tree_iterator(_Base_ptr n) {
+                _node_base = n;
+            }
+
+            _RB_tree_iterator(_Node_Ptr n) {
+                _node_base = n;
+            }
+
+            _RB_tree_iterator(const _RB_tree_iterator& other) {
+                _node_base = other._node_base;
+            }
+
+            _RB_tree_iterator(_RB_tree_iterator&& other) {
+                _node_base = other._node_base;
+            }
+
+            _Self& operator++() {
+                increment();
+                return *this;
+            }
+
+            _Self operator++(int) {
+                _Self tmp = *this;
+                increment();
+                return tmp;
+            }
+
+            _Self& operator--() {
+                decrement();
+                return *this;
+            }
+
+            _Self operator--(int) {
+                _Self tmp = *this;
+                decrement();
+                return tmp;
+            }
+
+            pointer operator->() {
+                return static_cast<_Node_Ptr>(_node_base)->_val_ptr();
+            }
+
+            reference operator*() {
+                return *static_cast<_Node_Ptr>(_node_base)->_val_ptr();
+            }
+
+        private:
+            void increment() {
+                if (_node_base->_right) {
+                    _node_base = _node_base->_right->_min();
+                } else {
+                    auto p = _node_base->_parent;
+                    while (p->_right == _node_base) {
+                        _node_base = p;
+                        p = p->_parent;
+                    }
+
+                    if (_node_base->_right != p) {
+                        // if we are incrementing on the maximum node of the tree, the result of _node_base will be the header
+                        // which will result in UB if dereferenced.
+                        _node_base = p;
+                    }
+                }
+            }
+
+            void decrement() {
+                if (_node_base->_left) {
+                    _node_base = _node_base->_left->_max();
+                } else {
+                    auto p = _node_base->_parent;
+                    while (p->_left == _node_base) {
+                        _node_base = p;
+                        p = p->_parent;
+                    }
+
+                    if (_node_base->_left != p) {
+                        _node_base = p;
+                    }
+                }
+            }
+
+
     };
 } // namespace MyStl
